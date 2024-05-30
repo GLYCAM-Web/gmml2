872411a4000a3de51e852054f98d527e005bd5d1
Tobias Grelsson <tgrelsson@gmail.com>
2024-05-30 14:28:35 +0200

    refactor: move helper functions closer to usage

--- includes/MolecularMetadata/glycoprotein.hpp
+++ includes/MolecularMetadata/glycoprotein.hpp
@@ -3,15 +3,11 @@
 
 #include <string>
 #include <vector>
-#include "includes/CentralDataStructure/residue.hpp"
 
 namespace glycoproteinMetadata
 {
     std::string LookupCodeForAminoAcidName(const std::string queryName);
     std::string LookupLinkTypeForAminoAcidName(const std::string queryName);
-    std::vector<std::string> GetTagsForSequence(const std::string firstRes = "", const std::string midRes = "",
-                                                const std::string lastRes = "");
-    std::string GetSequenceContextAndDetermineTags(cds::Residue* residue, std::vector<std::string>& tags);
     std::string ConvertGlycosylatedResidueName(const std::string queryname);
     std::string GetGlycositeConnectionAtomName(const std::string queryname);
 } // namespace glycoproteinMetadata
