4c576b46e4513765c08c048f3db8f46d8eea422a
Tobias Grelsson <tgrelsson@gmail.com>
2024-06-13 01:15:47 +0200

    vector contains function

--- includes/CodeUtils/find.hpp
+++ includes/CodeUtils/find.hpp
@@ -1,12 +1,19 @@
 #ifndef GMML_INCLUDES_CODEUTILS_FIND_HPP
 #define GMML_INCLUDES_CODEUTILS_FIND_HPP
 
+#include <algorithm>
 #include <string>
+#include <vector>
 #include <unordered_map>
 
 namespace codeUtils
 {
     std::string FindStringInStringMap(const std::string& s, const std::unordered_map<std::string, std::string>& sMap);
+
+    template<class T> bool contains(const std::vector<T> vector, const T element)
+    {
+        return std::find(vector.begin(), vector.end(), element) != vector.end();
+    }
 
 } // namespace codeUtils
 #endif // GMML_INCLUDES_CODEUTILS_STRINGS_HPP
