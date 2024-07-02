98787309130edcc3388fd0d3550fc9e8e1a68f9e
Tobias Grelsson <tgrelsson@gmail.com>
2024-07-02 17:49:43 +0200

    bugfix: rename duplicated include guard. How did this ever work?

--- includes/CentralDataStructure/cdsFunctions/bondByDistance.hpp
+++ includes/CentralDataStructure/cdsFunctions/bondByDistance.hpp
@@ -1,5 +1,6 @@
-#ifndef INCLUDES_CENTRALDATASTRUCTURE_CDSFUNCTIONS_CDSFUNCTIONS_HPP_
-#define INCLUDES_CENTRALDATASTRUCTURE_CDSFUNCTIONS_CDSFUNCTIONS_HPP_
+#ifndef INCLUDES_CENTRALDATASTRUCTURE_CDSFUNCTIONS_BONDBYDISTANCE_HPP
+#define INCLUDES_CENTRALDATASTRUCTURE_CDSFUNCTIONS_BONDBYDISTANCE_HPP
+
 #include "includes/CentralDataStructure/atom.hpp"
 #include "includes/CentralDataStructure/residue.hpp"
 #include <vector>
@@ -13,4 +14,4 @@ namespace cds
     void distanceBondIntra(std::vector<cds::Residue*> residues);
     void distanceBondInter(std::vector<cds::Residue*> residues);
 } // namespace cds
-#endif /* INCLUDES_CENTRALDATASTRUCTURE_CDSFUNCTIONS_CDSFUNCTIONS_HPP_ */
+#endif /* INCLUDES_CENTRALDATASTRUCTURE_CDSFUNCTIONS_BONDBYDISTANCE_HPP */
