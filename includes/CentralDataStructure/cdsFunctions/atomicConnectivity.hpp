7f6700a349202944d124bdc0eed962528bbf1680
Tobias Grelsson <tgrelsson@gmail.com>
2024-07-11 00:00:37 +0200

    cleanup: move functions to anonymous namespace

--- includes/CentralDataStructure/cdsFunctions/atomicConnectivity.hpp
+++ includes/CentralDataStructure/cdsFunctions/atomicConnectivity.hpp
@@ -3,12 +3,10 @@
 
 #include "includes/CentralDataStructure/residue.hpp"
 
+#include <vector>
+
 namespace cds
 {
-    void setBondingForAminoAcid(cds::Residue* proteinRes);
-    bool autoConnectSuccessiveResidues(cds::Residue* cTermRes, cds::Residue* nTermRes);
-    void setProteinIntraConnectivity(std::vector<cds::Residue*> proteinResidues);
-    void setProteinInterConnectivity(std::vector<cds::Residue*> proteinResidues);
     void setIntraConnectivity(std::vector<cds::Residue*> residues);
     void setInterConnectivity(std::vector<cds::Residue*> residues);
 } // namespace cds
