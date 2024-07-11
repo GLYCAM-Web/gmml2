7f40590bb58f56a52ed2c1a97218d3c07a9e399d
Preston <prestonj@uga.edu>
2024-07-11 11:55:09 -0400

    small header file cleanup
    removal of  a header file and addition of one for includes directives. The constants.hpp being added to teh pdbpreprocessorinputs.hpp may eventually not be needed but computer gave some grumbles regarding it when trying some brute force compiling dealios

--- includes/gmml.hpp
+++ includes/gmml.hpp
@@ -6,7 +6,6 @@
 #include "CodeUtils/logging.hpp"
 #include "MolecularMetadata/GLYCAM/bondlengthbytypepair.hpp"
 #include "MolecularMetadata/GLYCAM/glycam06residueinfo.hpp"
-#include "MolecularMetadata/GLYCAM/amberatomtypeinfo.hpp"
 #include "MolecularMetadata/GLYCAM/dihedralangledata.hpp"
 
 #endif // GMML_HPP
