02203ecb3629b93fcdc39707b7b2ccac5d7ed21e
Tobias Grelsson <tgrelsson@gmail.com>
2024-10-15 18:20:16 +0200

    refactor gpbuilder file reading and fix crlf bug

--- src/CentralDataStructure/InternalPrograms/GlycoproteinBuilder/gpInputStructs.cpp
+++ src/CentralDataStructure/InternalPrograms/GlycoproteinBuilder/gpInputStructs.cpp
@@ -1,80 +1,69 @@
 #include "includes/CentralDataStructure/InternalPrograms/GlycoproteinBuilder/gpInputStructs.hpp"
 #include "includes/CodeUtils/logging.hpp"
 #include "includes/CodeUtils/files.hpp"
-#include "includes/CodeUtils/directories.hpp"
 #include "includes/CodeUtils/strings.hpp"
 
 #include <vector>
 #include <string>
-#include <fstream>
-#include <algorithm> // remove
 
 using glycoprotein::GlycoproteinBuilderInputs;
 
 GlycoproteinBuilderInputs glycoprotein::readGPInputFile(std::string inputFileName)
 {
-    //    std::cout << "About to read " << inputFileName << std::endl << std::flush;
-    std::ifstream infile(inputFileName);
-    if (!infile)
-    {
-        std::string message = "Uh oh, input file: " + inputFileName + ", could not be opened for reading!\n";
-        gmml::log(__LINE__, __FILE__, gmml::ERR, message);
-        throw std::runtime_error(message);
-    }
+    bool readingGlycanSection = false;
     GlycoproteinBuilderInputs gpInputs;
-    while (infile) // While there's still stuff left to read
+    auto processLine = [&](const std::string& line)
     {
-        std::string strInput;
-        getline(infile, strInput);
-        strInput.erase(std::remove(strInput.begin(), strInput.end(), '\r'),
-                       strInput.end()); // files created in windows, being read in unix.
-        gmml::log(__LINE__, __FILE__, gmml::INF, strInput);
-        if (codeUtils::startsWith(strInput, "Protein:"))
+        if (readingGlycanSection)
         {
-            gpInputs.substrateFileName = codeUtils::split(strInput, ':').at(1);
-        }
-        if (codeUtils::startsWith(strInput, "NumberOfOutputStructures:"))
-        {
-            gpInputs.number3DStructures = std::stoul(codeUtils::split(strInput, ':').at(1));
-        }
-        if (codeUtils::startsWith(strInput, "maxThreads:"))
-        {
-            gpInputs.maxThreads = std::stoul(codeUtils::split(strInput, ':').at(1));
-        }
-        if (codeUtils::startsWith(strInput, "persistCycles:"))
-        {
-            gpInputs.persistCycles = std::stoul(codeUtils::split(strInput, ':').at(1));
-        }
-        if (codeUtils::startsWith(strInput, "freezeGlycositeResidueConformation:"))
-        {
-            gpInputs.freezeGlycositeResidueConformation = codeUtils::split(strInput, ':').at(1) == "true";
-        }
-        if (codeUtils::startsWith(strInput, "seed:"))
-        {
-            gpInputs.isDeterministic = true;
-            gpInputs.seed            = std::stoul(codeUtils::split(strInput, ':').at(1));
-        }
-        if (codeUtils::startsWith(strInput, "skipMDPrep:"))
-        {
-            gpInputs.skipMDPrep = codeUtils::split(strInput, ':').at(1) == "true";
-        }
-        if (codeUtils::startsWith(strInput, "ProteinResidue, GlycanName:"))
-        {
-            std::string tempBuffer; //  Temporarily holds whatever getline() finds on the line;
-            while (getline(infile, tempBuffer))
+            if (line == "END")
             {
-                tempBuffer.erase(std::remove(tempBuffer.begin(), tempBuffer.end(), '\r'),
-                                 tempBuffer.end()); // files created in windows, being read in unix.
-                if (tempBuffer == "END")
-                {
-                    break;
-                }
-                std::vector<std::string> splitLine = codeUtils::split(tempBuffer, '|');
+                readingGlycanSection = false;
+            }
+            else
+            {
+                std::vector<std::string> splitLine = codeUtils::split(line, '|');
                 gpInputs.glycositesInputVector.emplace_back(splitLine.at(0), splitLine.at(1));
             }
         }
-    }
-    //    std::cout << "Reading input file complete, just making a quick check\n" << std::flush;
+        else
+        {
+            if (codeUtils::startsWith(line, "ProteinResidue, GlycanName:"))
+            {
+                readingGlycanSection = true;
+            }
+            if (codeUtils::startsWith(line, "Protein:"))
+            {
+                gpInputs.substrateFileName = codeUtils::split(line, ':').at(1);
+            }
+            if (codeUtils::startsWith(line, "NumberOfOutputStructures:"))
+            {
+                gpInputs.number3DStructures = std::stoul(codeUtils::split(line, ':').at(1));
+            }
+            if (codeUtils::startsWith(line, "maxThreads:"))
+            {
+                gpInputs.maxThreads = std::stoul(codeUtils::split(line, ':').at(1));
+            }
+            if (codeUtils::startsWith(line, "persistCycles:"))
+            {
+                gpInputs.persistCycles = std::stoul(codeUtils::split(line, ':').at(1));
+            }
+            if (codeUtils::startsWith(line, "freezeGlycositeResidueConformation:"))
+            {
+                gpInputs.freezeGlycositeResidueConformation = codeUtils::split(line, ':').at(1) == "true";
+            }
+            if (codeUtils::startsWith(line, "seed:"))
+            {
+                gpInputs.isDeterministic = true;
+                gpInputs.seed            = std::stoul(codeUtils::split(line, ':').at(1));
+            }
+            if (codeUtils::startsWith(line, "skipMDPrep:"))
+            {
+                gpInputs.skipMDPrep = codeUtils::split(line, ':').at(1) == "true";
+            }
+        }
+    };
+    codeUtils::readFileLineByLine(inputFileName, processLine);
     if (gpInputs.glycositesInputVector.empty())
     {
         throw std::runtime_error(
