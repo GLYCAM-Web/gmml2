a359ef2ed340a6a48bc7bb8125ba9b6e14fa86a2
gitoliver <olgrant@uga.edu>
2025-03-05 16:48:02 +0100

    bugfix metadata overwriting itself with empty entry

--- src/MolecularMetadata/GLYCAM/dihedralangledata.cpp
+++ src/MolecularMetadata/GLYCAM/dihedralangledata.cpp
@@ -63,10 +63,19 @@ namespace
           // Omega angle in x-5 linkages.
           { "C.*"  , "O5"     , "Omg"  , -60.0  , AngleLimit{20.0, 20.0}  , 1.0   , RotamerType::permutation , "gg" , 3 , 1 , {}             , {"furanose"}                  , "O5" , "C5" , "C4" , "O4"  },
           { "C.*"  , "O5"     , "Omg"  ,  60.0  , AngleLimit{20.0, 20.0}  , 1.0   , RotamerType::permutation , "gt" , 3 , 2 , {}             , {"furanose"}                  , "O5" , "C5" , "C4" , "O4"  },
-          // Omega angle in x-1 linkages in ketose, furanoses
+		  // Ketose 1-1 linkages. Copied from old GLYCAM-Web builder.
+ 		  { "C1"   , "O1"     , "Phi"  , 180.0  , AngleLimit{20.0, 20.0}  , 1.0   , RotamerType::permutation , ""   , 1 , 1 , {}             , {"ketose"}                , "C2" , "C1" , "O1" , "C1" },
+  		  { "C1"   , "O1"     , "Psi"  ,-150.0  , AngleLimit{20.0, 20.0}  , 1.0   , RotamerType::permutation , ""   , 2 , 1 , {}             , {"ketose"}                , "C1" , "O1" , "C1" , "C2" },
+  		  { "C1"   , "O1"     , "Omg"  ,  60.0  , AngleLimit{20.0, 20.0}  , 1.0   , RotamerType::permutation , "gt" , 3 , 1 , {}             , {"ketose"}                , "O1" , "C1" , "C2" , "O5" },
+		  // Omega angle in x-1 linkages in ketose, furanoses
           { "C.*"  , "O1"     , "Omg"  , -60.0  , AngleLimit{20.0, 20.0}  , 1.0   , RotamerType::permutation , "gg" , 3 , 1 , {}             , {"furanose", "ketose"}        , "O1" , "C1" , "C2" , "O6"  },
           { "C.*"  , "O1"     , "Omg"  ,  60.0  , AngleLimit{20.0, 20.0}  , 1.0   , RotamerType::permutation , "gt" , 3 , 2 , {}             , {"furanose", "ketose"}        , "O1" , "C1" , "C2" , "O6"  },
-          // 2-7 linkages copied from GlycamWeb Jan 2021. Branching in the linkage causes oddities with the bond number and which atoms are chosen for the torsion.
+		  // Sucrose from: Bock K, Lemieux RU. Carbohydrate Research, 100 (1982) 63-74. Now allowing ordering of sequence either way, thus four entries instead of two.
+		  { "C1"   , "O2"     , "Phi"  ,-135.0  , AngleLimit{ 5.0,  5.0}  , 1.0   , RotamerType::permutation , ""   , 1 , 1 , {"pyranose", "aldose"}    , {"furanose", "ketose"}         , "C2" , "C1" , "O2" , "C2" },
+		  { "C1"   , "O2"     , "Psi"  ,  80.0  , AngleLimit{10.0, 10.0}  , 1.0   , RotamerType::permutation , ""   , 2 , 1 , {"pyranose", "aldose"}    , {"furanose", "ketose"}         , "C1" , "O2" , "C2" , "C1" },
+		  { "C2"   , "O1"     , "Phi"  ,  80.0  , AngleLimit{10.0, 10.0}  , 1.0   , RotamerType::permutation , ""   , 1 , 1 , {"furanose", "ketose"}    , {"pyranose", "aldose"}         , "C1" , "C2" , "O1" , "C1" },
+		  { "C2"   , "O1"     , "Psi"  ,-135.0  , AngleLimit{ 5.0,  5.0}  , 1.0   , RotamerType::permutation , ""   , 2 , 1 , {"furanose", "ketose"}    , {"pyranose", "aldose"}         , "C2" , "O1" , "C1" , "C2" },
+		  // 2-7 linkages copied from GlycamWeb Jan 2021. Branching in the linkage causes oddities with the bond number and which atoms are chosen for the torsion.
           { "C2"   , "O7"     , "Phi"  , -60.0  , AngleLimit{20.0, 20.0}  , 1.0   , RotamerType::permutation , "-g" , 1 , 2 , {"ulosonate", "alpha"}  , {"ulosonate"}    , "C3" , "C2" , "O." , "C."  },
           { "C2"   , "O7"     , "Psi"  ,   0.0  , AngleLimit{20.0, 20.0}  , 1.0   , RotamerType::permutation , "c"  , 2 , 1 , {"ulosonate", "alpha"}  , {"ulosonate"}    , "C." , "O." , "C." , "H."  },
           { "C.*"  , "O7"     , "Omg7" ,  60.0  , AngleLimit{20.0, 20.0}  , 1.0   , RotamerType::permutation , "g"  , 3 , 1 , {}                      , {"ulosonate"}    , "O7" , "C7" , "C6" , "O6"  },
@@ -135,15 +144,7 @@ namespace
           { "C2"   , "O8"     , "Omg8" ,-161.3  ,   AngleStd{20.0, 20.0}  , 0.07  , RotamerType::conformer   , "E"  , 3 , 5 , {"ulosonate", "alpha", "external"}  , {"ulosonate"}    , "O8" , "C8" , "C7" , "C6"  },
           { "C2"   , "O8"     , "Omg7" , -54.3  ,   AngleStd{20.0, 20.0}  , 0.07  , RotamerType::conformer   , "E"  , 4 , 5 , {"ulosonate", "alpha", "external"}  , {"ulosonate"}    , "C8" , "C7" , "C6" , "O6"  },
           { "C2"   , "O8"     , "Omg9" ,  71.1  ,   AngleStd{20.0, 20.0}  , 0.07  , RotamerType::conformer   , "E"  , 5 , 5 , {"ulosonate", "alpha", "external"}  , {"ulosonate"}    , "O9" , "C9" , "C8" , "O8"  },
-          // Sucrose from: Bock K, Lemieux RU. Carbohydrate Research, 100 (1982) 63-74. Now allowing ordering of sequence either way, thus four entries instead of two.
-          { "C1"   , "O2"     , "Phi"  ,-135.0  , AngleLimit{ 5.0,  5.0}  , 1.0   , RotamerType::permutation , ""   , 1 , 1 , {"pyranose", "aldose"}    , {"furanose", "ketose"}         , "C2" , "C1" , "O2" , "C2" },
-          { "C1"   , "O2"     , "Psi"  ,  80.0  , AngleLimit{10.0, 10.0}  , 1.0   , RotamerType::permutation , ""   , 2 , 1 , {"pyranose", "aldose"}    , {"furanose", "ketose"}         , "C1" , "O2" , "C2" , "C1" },
-          { "C2"   , "O1"     , "Phi"  ,  80.0  , AngleLimit{10.0, 10.0}  , 1.0   , RotamerType::permutation , ""   , 1 , 1 , {"furanose", "ketose"}    , {"pyranose", "aldose"}         , "C1" , "C2" , "O1" , "C1" },
-          { "C2"   , "O1"     , "Psi"  ,-135.0  , AngleLimit{ 5.0,  5.0}  , 1.0   , RotamerType::permutation , ""   , 2 , 1 , {"furanose", "ketose"}    , {"pyranose", "aldose"}         , "C2" , "O1" , "C1" , "C2" },
-          // Ketose 1-1 linkages. Copied from old GLYCAM-Web builder.
-          { "C1"   , "O1"     , "Phi"  , 180.0  , AngleLimit{20.0, 20.0}  , 1.0   , RotamerType::permutation , ""   , 1 , 1 , {}             , {"ketose"}                , "C2" , "C1" , "O1" , "C1" },
-          { "C1"   , "O1"     , "Psi"  ,-150.0  , AngleLimit{20.0, 20.0}  , 1.0   , RotamerType::permutation , ""   , 2 , 1 , {}             , {"ketose"}                , "C1" , "O1" , "C1" , "C2" },
-          { "C1"   , "O1"     , "Omg"  ,  60.0  , AngleLimit{20.0, 20.0}  , 1.0   , RotamerType::permutation , ""   , 3 , 1 , {}             , {"ketose"}                , "O1" , "C1" , "C2" , "O5" },
+
         // Common sugar derivatives
         // Phosphate/sulfate
           { "[SP]1", "N[2]"   , "Phi"  , 180.0  , AngleLimit{20.0, 20.0}  , 1.0   , RotamerType::permutation , ""   , 1 , 1 , {"derivative"}       , {"monosaccharide"}                  , "O." , ".1" , "N." , "C."  },
