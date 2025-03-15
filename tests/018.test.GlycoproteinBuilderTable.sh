ac6a30e33ca1f56fec8252ea1583edcfb16f7d26
Tobias Grelsson <tgrelsson@gmail.com>
2025-03-15 02:00:33 +0100

    tests: test various formats of gp builder table

--- tests/018.test.GlycoproteinBuilderTable.sh
+++ tests/018.test.GlycoproteinBuilderTable.sh
@@ -8,16 +8,34 @@ if [[ "${GMML_ROOT_DIR}" != *"gmml2" ]]; then
     exit 1
 fi
 
+directory="018"
+output="output/${directory}"
+expected="tests/correct_outputs/${directory}"
+rm -r "${output}" >/dev/null 2>&1
+mkdir -p "${output}"
+
 printf "Testing 018.test.createGlycosylationTables.cpp... "
-eval "${BIN_PATH} tests/inputs/018.4mbzEdit.pdb >018.output_GlycoproteinBuilderTable.txt 2>&1"
-if ! cmp 018.output_GlycoproteinBuilderTable.txt tests/correct_outputs/018.output_GlycoproteinBuilderTable.txt >/dev/null 2>&1; then
-    printf "Test FAILED!. tests/correct_outputs/018.output_GlycoproteinBuilderTable.txt different from 018.output_GlycoproteinBuilderTable.txt\ndiff tests/correct_outputs/018.output_GlycoproteinBuilderTable.txt 018.output_GlycoproteinBuilderTable.txt\n"
-    diff tests/correct_outputs/018.output_GlycoproteinBuilderTable.txt 018.output_GlycoproteinBuilderTable.txt
+eval "${BIN_PATH} tests/inputs/018.4mbzEdit.pdb --format list > ${output}/GlycoproteinBuilderTableList.txt 2>&1"
+eval "${BIN_PATH} tests/inputs/018.4mbzEdit.pdb --format csv > ${output}/GlycoproteinBuilderTable.csv 2>&1"
+eval "${BIN_PATH} tests/inputs/018.4mbzEdit.pdb --format txt > ${output}/GlycoproteinBuilderTable.txt 2>&1"
+eval "${BIN_PATH} tests/inputs/018.4mbzEdit.pdb --format html > ${output}/GlycoproteinBuilderTable.html 2>&1"
+
+if [ ! -d "${expected}" ]; then
+    echo "Test FAILED"
+    echo "directory ${expected} does not exist"
     echo "Exit Code: 1"
     return 1
-else
-    printf "Test passed.\n"
-    rm 018.output_GlycoproteinBuilderTable.txt
-    echo "Exit Code: 0"
-    return 0
 fi
+DIFF=$(diff -qr "${output}" "${expected}")
+if [ "$DIFF" ]
+then
+    echo "Test FAILED"
+    echo "${DIFF}"
+    echo "Exit Code: 1"
+    return 1
+fi
+
+rm -r "${output}" >/dev/null 2>&1
+printf "Test passed.\n"
+echo "Exit Code: 0"
+return 0
