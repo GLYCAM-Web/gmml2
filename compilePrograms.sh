4e8fd85cce474b7b1b32c9298871ef68f3302b3c
Tobias Grelsson <tgrelsson@gmail.com>
2025-08-01 01:16:44 +0200

    compile programs in alphabetical order

--- compilePrograms.sh
+++ compilePrograms.sh
@@ -10,18 +10,18 @@ LIB_PATH="${GMML_ROOT_DIR}/lib/"
 
 mkdir -p "${GMML_ROOT_DIR}/bin/"
 
-echo -e "\n${INFO_STYLE}###### Compiling internal programs ######${RESET_STYLE}"
+echo -e "\n${INFO_STYLE}###### Compiling programs ######${RESET_STYLE}"
+echo -e "Carbohydrate builder"
+g++ -std=c++17 -g -I ./ -L"${LIB_PATH}" -Wl,-rpath,"${LIB_PATH}" programs/CarbohydrateBuilder/carbBuilderMain.cpp -lgmml2 -pthread -o bin/carbohydrateBuilder
+echo -e "Draw glycan"
+g++ -std=c++17 -g -I ./ -L"${LIB_PATH}" -Wl,-rpath,"${LIB_PATH}" programs/DrawGlycan/drawGlycanMain.cpp -lgmml2 -pthread -o bin/drawGlycan
+echo -e "Glycomimetic preprocessor"
+g++ -std=c++17 -g -I ./ -L"${LIB_PATH}" -Wl,-rpath,"${LIB_PATH}" programs/glycomimeticPreprocessor.cpp -lgmml2 -pthread -o bin/glycomimeticPreprocessor
 echo -e "Glycoprotein builder"
 g++ -std=c++17 -g -I ./ -L"${LIB_PATH}" -Wl,-rpath,"${LIB_PATH}" programs/GlycoproteinBuilder/gpBuilder_main.cpp -lgmml2 -pthread -o bin/glycoproteinBuilder
 echo -e "Glycosylation site finder"
 g++ -std=c++17 -g -I ./ -L"${LIB_PATH}" -Wl,-rpath,"${LIB_PATH}" programs/createGlycosylationTables.cpp -lgmml2 -pthread -o bin/glycosylationSiteFinder
-echo -e "Carbohydrate builder"
-g++ -std=c++17 -g -I ./ -L"${LIB_PATH}" -Wl,-rpath,"${LIB_PATH}" programs/CarbohydrateBuilder/carbBuilderMain.cpp -lgmml2 -pthread -o bin/carbohydrateBuilder
-echo -e "Wiggle to site"
-g++ -std=c++17 -g -I ./ -L"${LIB_PATH}" -Wl,-rpath,"${LIB_PATH}" programs/WiggleToSite/wiggleToSiteDriver.cpp -lgmml2 -pthread -o bin/wiggleToSite
-echo -e "Draw glycan"
-g++ -std=c++17 -g -I ./ -L"${LIB_PATH}" -Wl,-rpath,"${LIB_PATH}" programs/DrawGlycan/drawGlycanMain.cpp -lgmml2 -pthread -o bin/drawGlycan
 echo -e "PDB Analyzer"
 g++ -std=c++17 -g -I ./ -L"${LIB_PATH}" -Wl,-rpath,"${LIB_PATH}" programs/pdbAnalyzer.cpp -lgmml2 -pthread -o bin/pdbAnalyzer
-echo -e "Glycomimetic preprocessor"
-g++ -std=c++17 -g -I ./ -L"${LIB_PATH}" -Wl,-rpath,"${LIB_PATH}" programs/glycomimeticPreprocessor.cpp -lgmml2 -pthread -o bin/glycomimeticPreprocessor
+echo -e "Wiggle to site"
+g++ -std=c++17 -g -I ./ -L"${LIB_PATH}" -Wl,-rpath,"${LIB_PATH}" programs/WiggleToSite/wiggleToSiteDriver.cpp -lgmml2 -pthread -o bin/wiggleToSite
